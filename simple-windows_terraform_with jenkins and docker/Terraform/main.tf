c9ac3a05d5737d5bf31c64c764e9754ed733e5ca
Raja4123 <rajachowdary412@gmail.com>
2025-07-08 23:01:25 +0530

    Update main.tf

--- simple-windows_terraform_with jenkins and docker/Terraform/main.tf
+++ simple-windows_terraform_with jenkins and docker/Terraform/main.tf
@@ -9,7 +9,17 @@ resource "aws_instance" "web" {
   
 
   provisioner "remote-exec" {
-      script = "install_docker.bat"
+      "sudo apt update -y",
+      "sudo apt install -y ca-certificates curl gnupg lsb-release",
+      "sudo mkdir -p /etc/apt/keyrings",
+      "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg",
+      "echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" |  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
+      "sudo apt update -y",
+      "sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin",
+      "sudo systemctl enable docker",
+      "sudo systemctl start docker",
+      "sudo usermod -aG docker ubuntu",
+      "sudo docker run -d -p 80:3000 raja4123/simple-app"
 
   connection {
     type        = "ssh"
